49ad919c609da910f07519d6ede0e0805e806c59
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-09-13 11:57:13 +0300

    WE make a request from our api

--- app/request.py
+++ app/request.py
@@ -1,4 +1,9 @@
+from app.news_test import News
 from app import app
+import urllib.request,json
+from .models import news
+
+News = news.News
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
