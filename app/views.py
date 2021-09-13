4d560d496ab0103cdad8ff85d9264e92c2a40127
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-09-13 10:58:05 +0300

    added a dynamic route for the news

--- app/views.py
+++ app/views.py
@@ -10,3 +10,10 @@ def index():
     '''
     message = 'Hello World'
     return render_template('index.html',message = message)
+@app.route('/news/<news_id>')
+def movie(news_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('movie.html',id = news_id)
