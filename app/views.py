718867aa9e9f7b152bcd9f5cdcb4147fdc34a535
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-09-14 08:41:29 +0300

    deployment to heroku

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from newsapi import NewsApiClient
 
 # Views
 @app.route('/')
@@ -8,12 +9,25 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    title = 'Home - Welcome to The latest world news'
-    return render_template('index.html', title = title)
-@app.route('/news/<int:news_id>')
-def movie(news_id):
-
-    '''
-    View movie page function that returns the movie details page and its data
-    '''
-    return render_template('news.html',id = news_id)
+    title = 'Home - Welcome to The latest news'
+    newsapi = NewsApiClient(api_key='91a623008e664b6bb43be5edc7dfa6ec')
+    
+    top_headlines = newsapi.get_top_headlines(sources='bbc-news,the-verge')
+    
+    articles = top_headlines['articles']
+    
+    desc = []
+    news = []
+    img = []
+    
+    
+    for i in range (len(articles)):
+        myarticles = articles[i]
+        
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img .append(myarticles['urlToImage'])
+        
+        
+    mylist = zip(news,desc,img)    
+    return render_template('index.html', context=mylist)
